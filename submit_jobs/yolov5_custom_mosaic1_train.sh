c9c32d1bcda162194b4fc9633ad4698484d57849
IshanChaks91 <ishan.chakravorty@gmail.com>
2022-09-02 04:53:29 +0200

    uploaded new submit job script

--- submit_jobs/yolov5_custom_mosaic1_train.sh
+++ submit_jobs/yolov5_custom_mosaic1_train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --partition=gpu          # partition (queue)
 #SBATCH --ntasks-per-node=64     # number of cores per node
-#SBATCH --mem=128G               # memory per node in MB (different units with suffix K|M|G|T)
+#SBATCH --mem=180G               # memory per node in MB (different units with suffix K|M|G|T)
 #SBATCH --time=1-00:00:00        # total runtime of job allocation (format D-HH:MM:SS; first parts optional)
-#SBATCH --output=yolov5_bdd10k_mosaicyolov5pytorch_my_mosaic_seed450_test11_v1.%j.out    # filename for STDOUT (%N: nodename, %j: job-ID)
-#SBATCH --error=yolov5_bdd10k_mosaicyolov5pytorch_my_mosaic_seed450_test11_v1.%j.err     # filename for STDERR hm  
+#SBATCH --output=yolov5_bdd10k_mosaicyolov5pytorch_my_mosaic_1000samples_seed450_test12_v1.%j.out    # filename for STDOUT (%N: nodename, %j: job-ID)
+#SBATCH --error=yolov5_bdd10k_mosaicyolov5pytorch_my_mosaic_1000samples_seed450_test12_v1.%j.err     # filename for STDERR hm  
 
 # here comes the part with the description of the computational work, for example:
 # load the OpenMPI environment
@@ -29,5 +29,5 @@ cd ~/my_projects/BDD10-DA-Yolov5/BDD10-DA-Yolov5/yolov5
 
 python3 ~/my_projects/BDD10-DA-Yolov5/BDD10-DA-Yolov5/yolov5/train.py --img 1280 --batch 16 --epochs 300 --seed 450 --patience 50 \
 	--hyp ~/my_projects/BDD10-DA-Yolov5/BDD10-DA-Yolov5/yolov5/data/hyps/hyp.scratch-low.yaml \
-	--data /scratch/ichakr2s/yolov5pytorch_aug/mosaic_bdd10k/mosaic_aug/data.yaml \
+	--data /scratch/ichakr2s/yolov5pytorch_aug/mosaic_bdd10k/Mosaic_Aug/data.yaml \
 	--weights yolov5s6.pt --cache
